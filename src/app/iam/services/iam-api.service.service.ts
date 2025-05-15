0722f4f132b50210daa0427dc2dc5d8d19f715a0
pierotm <pierotarazona822@gmail.com>
2025-05-15 17:25:18 -0500

    fix: api service for iam

--- src/app/iam/services/iam-api.service.service.ts
+++ src/app/iam/services/iam-api.service.service.ts
@@ -10,16 +10,15 @@ export class IamApiService {
   //private baseUrl = 'https://app-241114092351funda.azurewebsites.net/api/profiles';
   private baseUrl = 'https://movi-gestion.free.beeceptor.com/api/profile';
   constructor(private http: HttpClient) {}
-
+  
   // Autenticar al usuario con email y password
-  authenticateUser(email: string, lastName: string): Observable<UserEntity[]> {
-    const url = `${this.baseUrl}?email=${email}&lastName=${lastName}`;
-    return this.http.get<UserEntity[]>(url);
+  authenticateUser(email: string, password: string): Observable<UserEntity> {
+    return this.http.get<UserEntity>(`${this.baseUrl}/email/${email}/password/${password}`);
   }
 
   // Encontrar un usuario por email
   findUserWithEmail(email: string): Observable<UserEntity> {
-    return this.http.get<UserEntity>(`${this.baseUrl}?email=${email}`);
+    return this.http.get<UserEntity>(`${this.baseUrl}/email/${email}`);
   }
 
   // Crear un nuevo usuario
@@ -28,21 +27,16 @@ export class IamApiService {
   }
 
   // Encontrar un usuario por ID
-  findUserById(id: string): Observable<UserEntity[]> {
-    const url = `${this.baseUrl}?id=${id}`;
-    return this.http.get<UserEntity[]>(url);
+  findUserById(id: number): Observable<UserEntity> {
+    return this.http.get<UserEntity>(`${this.baseUrl}/${id}`);
   }
 
-  /**
-   * Actualizar usuario.
-   * Nota: Este endpoint no está simulado en Beeceptor actualmente.
-   *       Se mantiene como referencia para futura implementación.
-   */
   // Actualizar un usuario usando email y password
   updateUser(email: string, password: string, userUpdates: Partial<UserEntity>): Observable<UserEntity> {
     const url = `${this.baseUrl}/email/${email}/password/${password}`;
     return this.http.put<UserEntity>(url, userUpdates);
   }
+
 
   // Eliminar un usuario por ID
   deleteUser(id: number): Observable<void> {
