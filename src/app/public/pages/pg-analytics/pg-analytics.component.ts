523079817d14f7bdbca91000d30a67aa3689b8de
Mauricio Sebastian Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2025-05-16 04:44:26 -0500

    feat: add pg-analytics

--- src/app/public/pages/pg-analytics/pg-analytics.component.ts
+++ src/app/public/pages/pg-analytics/pg-analytics.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pg-analytics',
   templateUrl: './pg-analytics.component.html',
-  styleUrls: ['./pg-analytics.component.css'],
+  styleUrl: './pg-analytics.component.css'
+  
 })
 export class PgAnalyticsComponent {}
