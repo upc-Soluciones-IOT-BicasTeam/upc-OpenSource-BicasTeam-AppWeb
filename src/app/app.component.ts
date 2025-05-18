ded47ce935eed949d2dd17d7c45cab82e5c5261a
ELÍAS TORRES ESPINOZA <pierotarazona633@gmail.com>
2025-05-18 06:12:13 -0500

    feat: implementation of language change i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'upc-opensource-app';
+  supportedLangs = ['en','es'];
+  currentLang = 'en';
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+    this.translate.use(this.currentLang);
+  }
+
+  switchLang(lang: string): void {
+    this.currentLang = lang;
+    this.translate.use(lang);
+    console.log('switchLang →', lang);
+  }
+
+  toggleLang(): void {
+    const next = this.currentLang === 'en' ? 'es' : 'en';
+    console.log('toggleLang, next →', next);
+    this.switchLang(next);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// ngx-translate imports
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// Factory function for ngx-translate HTTP loader
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+// Application components & services
 import { IamLoginComponentComponent } from './iam/components/iam-login/iam-login.component.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { IamRegisterComponent } from './iam/components/iam-register/iam-register.component';
@@ -19,29 +28,12 @@ import { PgLoginComponent } from './public/pages/pg-login/pg-login.component';
 import { PgRegisterComponent } from './public/pages/pg-register/pg-register.component';
 import { PgRegisterInfoComponent } from './public/pages/pg-register-info/pg-register-info.component';
 import { PgRegisterSuccesComponent } from './public/pages/pg-register-succes/pg-register-succes.component';
-
-import {
-  MatSidenav,
-  MatSidenavContainer,
-  MatSidenavModule,
-} from '@angular/material/sidenav';
-import { MatListItem, MatListModule, MatNavList } from '@angular/material/list';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
-import { MatDivider, MatDividerModule } from '@angular/material/divider';
 import { ProfileHomeBusinessmanComponent } from './profiles-managment/components/profile-home-businessman/profile-home-businessman.component';
 import { ProfileHomeDriverComponent } from './profiles-managment/components/profile-home-driver/profile-home-driver.component';
 import { PgHomeDriverComponent } from './public/pages/pg-home-driver/pg-home-driver.component';
 import { PgHomeBusinessmanComponent } from './public/pages/pg-home-businessman/pg-home-businessman.component';
 import { VehicleBusinessmanComponent } from './service-execution/vehicles-management/components/vehicle-businessman/vehicle-businessman.component';
 import { PgVehiclesBusinessmanComponent } from './public/pages/pg-vehicles-businessman/pg-vehicles-businessman.component';
-import {
-  MatCard,
-  MatCardContent,
-  MatCardHeader,
-  MatCardSubtitle,
-  MatCardTitle,
-} from '@angular/material/card';
-import { MatButton } from '@angular/material/button';
 import { VehicleCarrierComponent } from './service-execution/vehicles-management/components/vehicle-carrier/vehicle-carrier.component';
 import { PgVehiclesCarrierComponent } from './public/pages/pg-vehicles-carrier/pg-vehicles-carrier.component';
 import { ProfileEditionComponent } from './profiles-managment/components/profile-edition/profile-edition.component';
@@ -57,6 +49,12 @@ import { PgStaffHomeComponent } from './public/pages/pg-staff-home/pg-staff-home
 import { SidebarStaffComponent } from './public/components/sidebar-staff/sidebar-staff.component';
 import { PgAnalyticsComponent } from './public/pages/pg-analytics/pg-analytics.component';
 import { AnalyticsComponent } from './analytics/components/analytics/analytics.component';
+import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
+import {MatSidenavModule} from "@angular/material/sidenav";
 
 @NgModule({
   declarations: [
@@ -93,16 +91,18 @@ import { AnalyticsComponent } from './analytics/components/analytics/analytics.c
     AnalyticsComponent,
   ],
   imports: [
-    HttpClientModule,
     BrowserModule,
+    HttpClientModule,
+    // ngx-translate module configuration
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     AppRoutingModule,
     FormsModule,
-    MatSidenavContainer,
-    MatNavList,
-    MatIcon,
-    MatDivider,
-    MatListItem,
-    MatSidenav,
     MatSidenavModule,
     MatListModule,
     MatIconModule,
@@ -110,12 +110,12 @@ import { AnalyticsComponent } from './analytics/components/analytics/analytics.c
     MatCard,
     MatCardTitle,
     MatCardContent,
-    MatButton,
     MatCardSubtitle,
     MatCardHeader,
+    MatButton,
     ReactiveFormsModule,
   ],
   providers: [provideAnimationsAsync(), IamApiService],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
