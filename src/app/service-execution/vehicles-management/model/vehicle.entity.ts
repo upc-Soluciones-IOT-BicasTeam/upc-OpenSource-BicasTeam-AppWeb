53e42fcb72f475e288e26f1214c420ccc74b62dd
MoisesD19 <moisesdonayre19@gmail.com>
2025-06-02 23:56:12 -0500

    feat(vehicles&tracking): deleted technical status

--- src/app/service-execution/vehicles-management/model/vehicle.entity.ts
+++ src/app/service-execution/vehicles-management/model/vehicle.entity.ts
@@ -3,11 +3,6 @@ export class VehicleEntity {
   userId: number;
   licensePlate: string;
   model: string;
-  engine: number;
-  fuel: number;
-  tires: number;
-  electricalSystem: number;
-  transmissionTemperature: number;
   driverName: string;
   vehicleImage?: string | null; // Permite que sea opcional o `null`
   color: string;
@@ -27,11 +22,6 @@ export class VehicleEntity {
     this.userId = 0;
     this.licensePlate = '';
     this.model = '';
-    this.engine = 0;
-    this.fuel = 0;
-    this.tires = 0;
-    this.electricalSystem = 0;
-    this.transmissionTemperature = 0;
     this.driverName = '';
     this.vehicleImage = null; // Puede ser `null` por defecto
     this.color = '';
